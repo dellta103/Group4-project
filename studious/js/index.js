c9d581c012682842db42acb9c5abd3dd57c3872a
thomasvuong2108 <vuongtanphat30@gmail.com>
2021-12-29 14:04:08 +0700

    fromPhat

---
+++ studious/js/index.js
@@ -0,0 +1,18 @@
+// Dark theme
+document.getElementById("switchButton").onclick = function () {
+  document.getElementById("myBody").classList.toggle("dark");
+};
+
+// Scroll
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header").style.padding = "30px 10px";
+    // document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("header").style.padding = "80px 10px";
+    // document.getElementById("logo").style.fontSize = "35px";
+  }
+}
